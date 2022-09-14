0162d51c7b32280130cd7aba35fc16ea5ce85a00
Timo <leuenberger.timo.1@gmail.com>
2022-09-14 19:46:24 +0200

    Initial commit

---
+++ src/test/java/com/biremues/BiremuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biremues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiremuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
